f44a84708a959d29123d3f8ed4815876127341c4
fjjav <fjjav1802@gmail.com>
2025-03-20 21:03:41 +0100

    Added by Colaborator

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -69,6 +69,12 @@ public class Date {
     this.year = year;
   }
 
+  public Date(int tag, int monat, int jahr) {
+    this.day = tag;
+    this.month = monat;
+    this.year = jahr;
+  }
+
   /**
    * Validates the date according to the predefined rules: the year must be within 1900 to 2050, the
    * month within 1 to 12, and the day valid within the given month and year, considering leap
