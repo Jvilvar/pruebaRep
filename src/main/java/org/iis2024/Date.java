af8aa542b28535e753a6f7fef8f532b6fa487336
Jvilvar <jvilvar0109@uma.es>
2025-03-20 20:57:59 +0100

    Changes commited

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,9 +20,8 @@ public class Date {
   private final int month;
   private final int year;
 
-
   public void methodAddedByOwner(){
-    System.out.println("Method added by the owner");
+    System.out.println("Method addedd by the owner");
   }
   /**
    * Retrieves the day component of this date.
